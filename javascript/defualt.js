f4c5d9b9f171abf19322d46589e7fdc00a9172ad
Jacob Humston <jhumston24@sciototech.org>
2023-09-27 14:27:09 -0400

    fixed some stuff!! improvements

--- javascript/defualt.js
+++ javascript/defualt.js
@@ -11,7 +11,38 @@ function changeTitle(document, title, name) {
     document.title = `${title} | ${name}`;
 }
 
+/**
+ * Wait for an element to be present/loaded via it's ID.
+ * @param {string} id ID of the element to wait for.
+ * @returns {Promise.<void>}
+ */
+function waitForElement(id) {
+    return new Promise(function (resolve) {
+        const interval = setInterval(function () {
+            if (document.getElementById(id) !== null) {
+                clearInterval(interval);
+                resolve();
+            }
+        });
+    });
+}
+
+/**
+ * Update the menu bar to the correct state.
+ */
+function updateMenuBar() {
+    const menubar = document.getElementById('menuBar');
+    const anchors = menubar.getElementsByClassName('menuBarNormal');
+    for (let i = 0; i !== anchors.length; i++) {
+        const item = anchors.item(i);
+        if (item.href === window.location.href) {
+            item.classList.add('menuBarNormalCurrent');
+        }
+    }
+}
+
 window.addEventListener('load', function () {
     changeTitle(document, document.title, 'Game Day Grill');
     libs.include.includeBody(document, 'includes/menuBar.html', 'start');
+    waitForElement('menuBar').then(updateMenuBar);
 });
