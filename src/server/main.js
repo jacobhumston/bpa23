55f9e77bc415494c10eea93e3a313a31c5c0ed95
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2024-02-20 15:13:26 +0000

    Updated server script to accept the first argument as the port. (Optional)

--- src/server/main.js
+++ src/server/main.js
@@ -1,4 +1,6 @@
 import express from 'express';
 const app = express();
+let port = 80;
+if (process.argv[2]) port = Number(process.argv[2]);
 app.use('/', express.static('src/client/', { extensions: ['html'] }));
-app.listen(80);
+app.listen(port, () => console.log(`Server listening on port ${port}.`));
